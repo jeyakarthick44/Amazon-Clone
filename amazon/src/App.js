13b1194f94fa0640e388df901566f42861d24703
jeyakarthick44 <jeyakarthick408@gmail.com>
2021-12-05 10:14:57 +0530

    Ui completed , next work on redux

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -3,16 +3,18 @@ import { Routes, Route} from 'react-router-dom'
 import Header from './Header';
 import Login from './Login';
 import Checkout from './Checkout';
+import Home from './Home';
 
 function App() {
   return (
     <div>
+<Header />
+<Home />
 <Routes>
   <Route  exact path="/" />
   <Route path="/checkout" element={<Checkout />} exact />
   <Route path="/login" element={<Login />}exact />
 </Routes>
-<Header />
   </div>
   );
 }
--- amazon/src/Header.js
+++ amazon/src/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
         <img
           className="header__logo"
           src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        />
+        alt=""/>
       </Link>
 
       <div className="header__search">
@@ -20,10 +20,10 @@ const Header = () => {
       </div>
 
       <div className="header__nav">
-        <Link to={!user && '/login'}>
-          <div onClick={handleAuthenticaton} className="header__option">
-            <span className="header__optionLineOne">Hello {!user ? 'Guest' : user.email}</span>
-            <span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
+        <Link to='/'>
+          <div className="header__option">
+            <span className="header__optionLineOne">Login</span>
+            <span className="header__optionLineTwo">hi jk</span>
           </div>
         </Link>
 
@@ -44,7 +44,7 @@ const Header = () => {
           <div className="header__optionBasket">
             <ShoppingBasketIcon />
             <span className="header__optionLineTwo header__basketCount">
-              {basket?.length}
+              {/* {basket?.length} */}
             </span>
           </div>
         </Link>
